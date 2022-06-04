f78a8903a2a2179e9eecfd120809dc81f338cceb
MaxMcRolly <m.prylutski@gmail.com>
2022-06-04 18:00:08 +0300

    Created bottom navigation bar

---
+++ gimme_the_loot/lib/home/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,28 @@
+import 'package:gimme_the_loot/home/logic/cubit/page_view_cubit.dart';
+import "package:gimme_the_loot/imports.dart";
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key,required this.index,required this.pageController,}) : super(key: key);
+  final int index;
+final PageController pageController;
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  late int selectedIndex;
+
+  @override
+  void initState() {
+selectedIndex=widget.index;
+    super.initState();
+  }
+  void _onItemTapped(int pageIndex){
+    widget.pageController.jumpToPage(pageIndex);
+    context.read<PageViewCubit>().changeCurrentPageIndex(pageIndex: pageIndex);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(onTap: _onItemTapped,currentIndex: context.read<PageViewCubit>().state.currentPageIndex,items: [BottomNavigationBarItem(icon: Icon(Icons.abc),),BottomNavigationBarItem(icon: Icon(Icons.abc_outlined),),BottomNavigationBarItem(icon: Icon(Icons.abc_rounded),),],);
+  }
+}
--- gimme_the_loot/lib/imports.dart
+++ gimme_the_loot/lib/imports.dart
@@ -1,3 +1,4 @@
 export "package:flutter/material.dart";
+export "package:flutter_bloc/flutter_bloc.dart";
 
-export 'package:gimme_the_loot/home/home_screen.dart';
+export 'package:gimme_the_loot/home/screens/home_screen.dart';
