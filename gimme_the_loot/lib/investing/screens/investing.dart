a92d558e30827ee448cf800f2430c9b27e9d4419
MaxMcRolly <m.prylutski@gmail.com>
2022-06-06 23:16:11 +0300

    Created investing screen

---
+++ gimme_the_loot/lib/investing/screens/investing.dart
@@ -0,0 +1,19 @@
+import "package:gimme_the_loot/imports.dart";
+
+class InvestingScreen extends StatefulWidget {
+  const InvestingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InvestingScreen> createState() => _InvestingScreenState();
+}
+
+class _InvestingScreenState extends State<InvestingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Investing"),
+      ),
+    );
+  }
+}
