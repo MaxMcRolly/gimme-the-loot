66af33fa7e5e01700a5eae052729ff5060efc868
MaxMcRolly <m.prylutski@gmail.com>
2022-06-06 23:14:58 +0300

    Added local imports to imports file

--- gimme_the_loot/lib/imports.dart
+++ gimme_the_loot/lib/imports.dart
@@ -1,7 +1,9 @@
 export "package:flutter/material.dart";
 export "package:flutter_bloc/flutter_bloc.dart";
 
+export 'package:gimme_the_loot/theme.dart';
 export 'package:gimme_the_loot/home/screens/home_screen.dart';
 
 export "package:gimme_the_loot/home/logic/cubit/page_view_cubit.dart";
 export 'package:gimme_the_loot/home/widgets/bottom_navigation_bar.dart';
+export 'package:gimme_the_loot/finance/screens/finances.dart';
