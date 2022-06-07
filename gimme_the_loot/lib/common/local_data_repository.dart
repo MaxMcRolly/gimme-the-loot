2079fdab3ae4808f7c193617a718f29f9dc9bc13
MaxMcRolly <m.prylutski@gmail.com>
2022-06-07 23:15:59 +0300

    Created repository for local data storage

---
+++ gimme_the_loot/lib/common/local_data_repository.dart
@@ -0,0 +1,10 @@
+import 'package:gimme_the_loot/finance/models/bank_account.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import "imports.dart";
+
+late SharedPreferences sharedPreferences;
+Future<void> initalizeLocalData() async {
+  sharedPreferences = await SharedPreferences.getInstance();
+  await BankAccount().readFromFile();
+}
