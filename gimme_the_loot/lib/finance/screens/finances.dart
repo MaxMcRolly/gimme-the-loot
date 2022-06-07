fffd3c83f9f5affdee671e7f5a88e351d4f37a87
MaxMcRolly <m.prylutski@gmail.com>
2022-06-07 23:32:39 +0300

    Added elements for debugging

--- gimme_the_loot/lib/finance/screens/finances.dart
+++ gimme_the_loot/lib/finance/screens/finances.dart
@@ -1,4 +1,5 @@
-import "package:gimme_the_loot/imports.dart";
+import 'package:gimme_the_loot/imports.dart';
+import 'package:gimme_the_loot/finance/models/bank_account.dart';
 
 class FinancesScreen extends StatefulWidget {
   const FinancesScreen({Key? key}) : super(key: key);
@@ -11,9 +12,39 @@ class _FinancesScreenState extends State<FinancesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Finances"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Finances"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(
+            right: 16.0,
+            left: 16.0,
+            top: 16.0,
+          ),
+          child: CustomScrollView(
+            slivers: [
+              SliverList(
+                delegate: SliverChildListDelegate.fixed(
+                  [
+                    //TODO: make total balance card
+                    TotalBalanceCard(),
+                    SizedBox(
+                      height: 16,
+                    ),
+                  ],
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    BankAccount bankAccount = bankAccounts[index];
+                    return BankAccountCard(bankAccount: bankAccount);
+                  },
+                  childCount: bankAccounts.length,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
