da60b044d841026ad8f4912e84f5002485f70ae1
MaxMcRolly <m.prylutski@gmail.com>
2022-06-07 23:16:44 +0300

    Added local data initializer to the start of the app

--- gimme_the_loot/lib/home/screens/home_screen.dart
+++ gimme_the_loot/lib/home/screens/home_screen.dart
@@ -1,5 +1,4 @@
-import "package:gimme_the_loot/imports.dart";
-import 'package:gimme_the_loot/investing/screens/investing.dart';
+import 'package:gimme_the_loot/common/imports.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -9,35 +8,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late bool _isLoaded = false;
+  @override
+  void initState() {
+    _isLoaded = false;
+    _loadData();
+    super.initState();
+  }
+
+  _loadData() async {
+    await initalizeLocalData();
+    setState(() {
+      _isLoaded = true;
+    });
+  }
+
   PageController homePageController =
       PageController(initialPage: 0, keepPage: true);
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PageViewCubit, PageViewState>(
       builder: (context, state) {
-        return Scaffold(
-          // appBar: AppBar(title: Text("Gimme the Loot")),
-          body: PageView(
-            onPageChanged: (value) {
-              context
-                  .read<PageViewCubit>()
-                  .changeCurrentPageIndex(pageIndex: value);
-            },
-            controller: homePageController,
-            children: [
-              FinancesScreen(),
-              InvestingScreen(),
-              Scaffold(
-                body: Center(
-                  child: Text("blank screen, nothing to see here"),
+        return _isLoaded
+            ? Scaffold(
+                // appBar: AppBar(title: Text("Gimme the Loot")),
+                body: PageView(
+                  onPageChanged: (value) {
+                    context
+                        .read<PageViewCubit>()
+                        .changeCurrentPageIndex(pageIndex: value);
+                  },
+                  controller: homePageController,
+                  children: [
+                    FinancesScreen(),
+                    InvestingScreen(),
+                    Scaffold(
+                      body: Center(
+                        child: Text("blank screen, nothing to see here"),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-          bottomNavigationBar: BottomNavBar(
-              index: state.currentPageIndex,
-              pageController: homePageController),
-        );
+                bottomNavigationBar: BottomNavBar(
+                    index: state.currentPageIndex,
+                    pageController: homePageController),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              );
       },
     );
   }
