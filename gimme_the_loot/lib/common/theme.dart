02bba0eb6f2fd4999f5662b1b8e2f1b467e584fe
MaxMcRolly <m.prylutski@gmail.com>
2022-06-08 23:20:25 +0300

    Added input field themesation to the theme file

--- gimme_the_loot/lib/common/theme.dart
+++ gimme_the_loot/lib/common/theme.dart
@@ -2,6 +2,20 @@ import '../imports.dart';
 
 ThemeData getTheme() {
   return ThemeData(
+    textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.deepOrange),
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.deepOrange),
+      ),
+      focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.deepOrange),
+      ),
+      hoverColor: Colors.deepOrange,
+      labelStyle: TextStyle(
+        color: Colors.deepOrange,
+      ),
+      focusColor: Colors.deepOrange,
+    ),
     scaffoldBackgroundColor: Colors.black,
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       backgroundColor: Colors.black,
